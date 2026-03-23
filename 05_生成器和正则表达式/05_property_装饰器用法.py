e11f620efe2d4f0d5103c7d4df112f8980214dc5
lwy9107124035 <2443647713@qq.com>
2026-03-23 18:54:46 +0800

    1

--- 05_生成器和正则表达式/05_property_装饰器用法.py
+++ 05_生成器和正则表达式/05_property_装饰器用法.py
@@ -1,2 +1,19 @@
 class Student:
+    def __init__(self):
+        self.__age = 18
+
+    @property
+    def age(self):
+        return self.__age
     
+    @age.setter
+    def age(self, age):
+        self.__age = age
+
+
+if __name__ == '__main__':
+    s = Student()
+    s.age = 19
+
+    print(s.age)
+
